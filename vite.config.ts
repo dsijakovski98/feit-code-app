c5d9af2376698b8e86bd88663f0b985d5fd8462b
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-08-25 00:58:01 +0200

    Fix font handling plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import vsharp from "vite-plugin-vsharp";
 import webFontDownload from "vite-plugin-webfont-dl";
 
-import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
   plugins: [
     react(),
-    webFontDownload(),
+    webFontDownload([
+      "https://fonts.googleapis.com/css2?family=Exo+2:wght@100..900&display=swap",
+      "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap",
+    ]),
     createSvgIconsPlugin({
       // Specify the icon folder to be cached
       iconDirs: [path.resolve(process.cwd(), "src/icons")],
