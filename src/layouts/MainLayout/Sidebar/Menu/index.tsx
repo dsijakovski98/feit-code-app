f57394bb4f3aa14435bdf89bddbcf3915e0c1b94
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-09-14 18:45:20 +0200

    Fix menu suspense components

--- src/layouts/MainLayout/Sidebar/Menu/index.tsx
+++ src/layouts/MainLayout/Sidebar/Menu/index.tsx
@@ -1,11 +1,12 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 
-import ProfessorMenu from "@/layouts/MainLayout/Sidebar/Menu/ProfessorMenu";
-import SidebarMenuSkeleton from "@/layouts/MainLayout/Sidebar/Menu/Skeleton";
 import StudentMenu from "@/layouts/MainLayout/Sidebar/Menu/StudentMenu";
 
 import { useFCUser } from "@/hooks/useFCUser";
 import { USER_TYPE } from "@/types";
+
+const ProfessorMenu = lazy(() => import("@/layouts/MainLayout/Sidebar/Menu/ProfessorMenu"));
+const SidebarMenuSkeleton = lazy(() => import("@/layouts/MainLayout/Sidebar/Menu/Skeleton"));
 
 const SidebarMenu = () => {
   const { userData } = useFCUser();
