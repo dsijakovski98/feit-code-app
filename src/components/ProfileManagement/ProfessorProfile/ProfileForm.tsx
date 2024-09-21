88eced6519ec9cc7890f016e18f639f776cd7e43
Daniel Shijakovski <dshijakovski@yahoo.com>
2024-09-21 15:27:01 +0200

    Fix department select styles

--- src/components/ProfileManagement/ProfessorProfile/ProfileForm.tsx
+++ src/components/ProfileManagement/ProfessorProfile/ProfileForm.tsx
@@ -112,8 +112,8 @@ const ProfessorProfileForm = ({ professor }: Props) => {
               >
                 {MAJORS.map(({ label, description }) => (
                   <SelectItem key={label} textValue={label}>
-                    <p className="text-base font-medium">{label}</p>
-                    <p className="text-sm text-foreground-300">{description}</p>
+                    <p className="text-base font-semibold">{label}</p>
+                    <p className="text-sm text-foreground-400">{description}</p>
                   </SelectItem>
                 ))}
               </Select>
