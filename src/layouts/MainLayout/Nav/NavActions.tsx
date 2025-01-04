6e345f99a6e5d9ca636854af0c63e90ab7094c8b
dsijakovski98 <dshijakovski@yahoo.com>
2025-01-04 15:38:45 +0100

    Remove notifications menu

--- src/layouts/MainLayout/Nav/NavActions.tsx
+++ src/layouts/MainLayout/Nav/NavActions.tsx
@@ -1,6 +1,5 @@
 import { Divider } from "@nextui-org/react";
 
-import NotificationsMenu from "@/layouts/MainLayout/Nav/NotificationsMenu";
 import UserAvatar from "@/layouts/MainLayout/Nav/UserAvatar";
 
 import ThemeSwitcher from "@/components/ui/ThemeSwitcher";
@@ -8,11 +7,7 @@ import ThemeSwitcher from "@/components/ui/ThemeSwitcher";
 const NavActions = () => {
   return (
     <div className="flex -translate-x-0.5 items-center gap-4">
-      <div className="space-x-2 lg:space-x-1">
-        <ThemeSwitcher />
-
-        <NotificationsMenu />
-      </div>
+      <ThemeSwitcher />
 
       <Divider orientation="vertical" className="mr-2 block !h-10 w-px self-stretch" />
 
